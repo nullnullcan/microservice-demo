c2c883ae6d1ab9ffca7df4de2d4dd3f03cc932c2
nullnullcan <jqjnh@126.com>
2026-06-02 16:23:06 +0800

    首次提交

---
+++ gateway/src/main/java/com/demo/client/UserFeignClient.java
@@ -0,0 +1,12 @@
+package com.demo.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(name = "user-service", path = "/internal")
+public interface UserFeignClient {
+    @GetMapping("/users")
+    List<String> getUserList();
+}
